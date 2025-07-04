b88e4caa0e610f6c4f98f5260163d3bd1122b0dd
w7153045 <w7153045@gmail.com>
2025-07-04 19:32:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,4 +10,4 @@ MONGODB: process.env.MONGODB || "mongodb://mongo:bxAukDlIDvhXTIirPpWddKujGOOHaAX
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
-}; 
+};   
